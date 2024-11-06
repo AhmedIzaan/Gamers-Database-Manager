5ceb4be62404b4acfe0d6a9e380dd237d0214207
AhmedIzaan <i230015@isb.nu.edu.pk.noreply.github.com>
2024-11-06 15:00:58 +0500

    Added search and deletion functionalities

--- ass.cpp
+++ ass.cpp
@@ -58,6 +58,7 @@ public:
         cout << "Password: " << Password << endl;
 
         Games_Played_class *currentGame = games_played;
+        cout << "Games played:\n";
         while (currentGame != nullptr)
         {
             cout << "  Game ID: " << currentGame->Game_ID << endl;
@@ -87,8 +88,17 @@ public:
         Publisher = publisher;
         File_Size_in_GBs = fileSize;
         Downloads = downloads;
-        left=nullptr;
-        right=nullptr;
+        left = nullptr;
+        right = nullptr;
+    }
+    void printGameInfo()
+    {
+        cout << "Game ID: " << Game_ID << endl;
+        cout << "Name: " << Name << endl;
+        cout << "Developer: " << Developer << endl;
+        cout << "Publisher: " << Publisher << endl;
+        cout << "File Size: " << File_Size_in_GBs << " GBs" << endl;
+        cout << "Downloads: " << Downloads << endl;
     }
 };
 class playerBST
@@ -110,6 +120,11 @@ public:
             return findPlayer(node->left, playerID);
         }
         return findPlayer(node->right, playerID);
+    }
+
+    Player *searchPlayer(const string &playerID)
+    {
+        return findPlayer(root, playerID);
     }
 
     void insertPlayer(Player *newNode)
@@ -138,6 +153,72 @@ public:
             insert(node->right, newNode);
         }
     }
+    void deletePlayer(const string &playerID)
+    {
+        root = deletePlayerNode(root, playerID);
+    }
+    Player *deletePlayerNode(Player *node, const string &playerID)
+    {
+        if (node == nullptr)
+            return nullptr;
+
+        if (playerID < node->Player_ID)
+        {
+            node->left = deletePlayerNode(node->left, playerID);
+        }
+        else if (playerID > node->Player_ID)
+        {
+            node->right = deletePlayerNode(node->right, playerID);
+        }
+        else
+        {
+
+            if (node->left == nullptr)
+            {
+                Player *temp = node->right;
+                deletePlayerGames(node);
+                delete node;
+                return temp;
+            }
+            else if (node->right == nullptr)
+            {
+                Player *temp = node->left;
+                deletePlayerGames(node);
+                delete node;
+                return temp;
+            }
+
+            Player *temp = findMinPlayer(node->right);
+            node->Player_ID = temp->Player_ID;
+            node->Name = temp->Name;
+            node->Phone_Number = temp->Phone_Number;
+            node->Email = temp->Email;
+            node->Password = temp->Password;
+
+            node->games_played = temp->games_played;
+            temp->games_played = nullptr;
+            node->right = deletePlayerNode(node->right, temp->Player_ID);
+        }
+        return node;
+    }
+    Player *findMinPlayer(Player *node)
+    {
+        Player *current = node;
+        while (current && current->left != nullptr)
+            current = current->left;
+        return current;
+    }
+
+    void deletePlayerGames(Player *player)
+    {
+        Games_Played_class *currentGame = player->games_played;
+        while (currentGame)
+        {
+            Games_Played_class *nextGame = currentGame->next;
+            delete currentGame;
+            currentGame = nextGame;
+        }
+    }
 };
 class GameBST
 {
@@ -157,6 +238,11 @@ public:
             return findGame(node->left, gameID);
         }
         return findGame(node->right, gameID);
+    }
+
+    Game *searchGame(const string &gameID)
+    {
+        return findGame(root, gameID);
     }
 
     void insertGame(Game *newNode)
@@ -183,6 +269,67 @@ public:
         else
         {
             insert(node->right, newNode);
+        }
+    }
+    void deleteGame(const string &gameID)
+    {
+        root = deleteGameNode(root, gameID);
+    }
+    Game *deleteGameNode(Game *node, const string &gameID)
+    {
+        if (node == nullptr)
+            return nullptr;
+
+        if (gameID < node->Game_ID)
+        {
+            node->left = deleteGameNode(node->left, gameID);
+        }
+        else if (gameID > node->Game_ID)
+        {
+            node->right = deleteGameNode(node->right, gameID);
+        }
+        else
+        {
+            if (node->left == nullptr)
+            {
+                Game *temp = node->right;
+                delete node;
+                return temp;
+            }
+            else if (node->right == nullptr)
+            {
+                Game *temp = node->left;
+                delete node;
+                return temp;
+            }
+
+            Game *temp = findMinGame(node->right);
+            node->Game_ID = temp->Game_ID;
+            node->Name = temp->Name;
+            node->Developer = temp->Developer;
+            node->Publisher = temp->Publisher;
+            node->File_Size_in_GBs = temp->File_Size_in_GBs;
+            node->Downloads = temp->Downloads;
+
+            node->right = deleteGameNode(node->right, temp->Game_ID);
+        }
+        return node;
+    }
+
+    Game *findMinGame(Game *node)
+    {
+        Game *current = node;
+        while (current && current->left != nullptr)
+            current = current->left;
+        return current;
+    }
+    void printGames(Game *node)
+    {
+        if (node != nullptr)
+        {
+            printGames(node->left);
+            cout << "Game ID: " << node->Game_ID << ", Name: " << node->Name << ", Developer: " << node->Developer << ", Publisher: " << node->Publisher << ", File Size: " << node->File_Size_in_GBs << " GBs, Downloads: " << node->Downloads << endl;
+            printGames(node->right);
         }
     }
 };
@@ -222,7 +369,6 @@ void readingDataFromPlayerData(const string &fileName, playerBST &playerTree)
 
         Player *newPlayer = new Player(playerID, name, phone, email, password);
 
-        
         string gameID, hoursStr, achievementsStr;
         while (getline(ss, gameID, ',') && getline(ss, hoursStr, ',') && getline(ss, achievementsStr, ','))
         {
@@ -268,15 +414,82 @@ void readingDataFromGameData(const string &fileName, GameBST &gameTree)
 
     gameFile.close();
 }
+void searchAndPrint(playerBST &playerTree, GameBST &gameTree)
+{
+    string searchPlayerID;
+    cout << "Enter Player ID to search: ";
+    cin >> searchPlayerID;
+    Player *foundPlayer = playerTree.searchPlayer(searchPlayerID);
+    if (foundPlayer != nullptr)
+    {
+        cout << "Player found:\n";
+        foundPlayer->printPlayerInfo();
+    }
+    else
+    {
+        cout << "Player with ID " << searchPlayerID << " not found.\n";
+    }
 
+    string searchGameID;
+    cout << "Enter Game ID to search: ";
+    cin >> searchGameID;
+    Game *foundGame = gameTree.searchGame(searchGameID);
+    if (foundGame != nullptr)
+    {
+        cout << "Game found:\n";
+        foundGame->printGameInfo();
+    }
+    else
+    {
+        cout << "Game with ID " << searchGameID << " not found.\n";
+    }
+}
+void deletePlayerAndGame(playerBST &playerTree, GameBST &gameTree)
+{
+    string deletePlayerID;
+    cout << "Enter Player ID to delete: ";
+    cin >> deletePlayerID;
+    playerTree.deletePlayer(deletePlayerID);
+    cout << "Player with ID " << deletePlayerID << " deleted.\n";
+
+    string deleteGameID;
+    cout << "Enter Game ID to delete: ";
+    cin >> deleteGameID;
+    gameTree.deleteGame(deleteGameID);
+    cout << "Game with ID " << deleteGameID << " deleted.\n";
+}
 int main()
 {
     playerBST playerTree;
     GameBST gameTree;
 
-    
     readingDataFromPlayerData("Players.txt", playerTree);
     readingDataFromGameData("Games.txt", gameTree);
 
+    int choice;
+    do
+    {
+        cout << "\nMenu:\n";
+        cout << "0. Exit\n";
+        cout << "1. Search Player and Game\n";
+        cout << "2.Delete Player and Game\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 0:
+            cout << "Exiting...\n";
+            break;
+        case 1:
+            searchAndPrint(playerTree, gameTree);
+            break;
+        case 2:
+            deletePlayerAndGame(playerTree, gameTree);
+            break;
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 0);
     return 0;
 }
